addb6d246db3cb76d8b20e5a5918e136d41be879
Simon Donike <accounts@donike.net>
2025-06-06 12:25:02 +0100

    opensr_utils demo

--- opensr_utils_demo.py
+++ opensr_utils_demo.py
@@ -1,13 +1,18 @@
-# This script is an example how opensr-utils can be used in unison with opensr-model.
+# This script is an example how opensr-utils can be used in unison with opensr-model
+# in order to SR a while S2 tile.
 
 # Import and Instanciate SR Model
 import opensr_model
 import torch
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = opensr_model.SRLatentDiffusionLightning(bands="10m",device=device) 
-model.load_pretrained("opensr_10m_v4_v6.ckpt")
+from omegaconf import OmegaConf
 
-# perform SR with opensr-utils
+device = "cuda" if torch.cuda.is_available() else "cpu"
+config = OmegaConf.load("opensr_model/configs/config_10m.yaml") # load config
+model = opensr_model.SRLatentDiffusion(config, device=device) # create model
+model.load_pretrained(config.ckpt_version) # load checkpoint
+
+
+# perform SR with opensr-utils on whole tile (.SAFE format)
 from opensr_utils.main import windowed_SR_and_saving
 path = "/data3/inf_data/S2A_MSIL2A_20241026T105131_N0511_R051_T30SYJ_20241026T150453.SAFE/"
 sr_obj = windowed_SR_and_saving(folder_path=path, window_size=(128, 128), factor=4, keep_lr_stack=True)
