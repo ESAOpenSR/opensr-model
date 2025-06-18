33345b59db83c77290df563c475e4ccd3bda1c1f
Simon Donike <accounts@donike.net>
2025-06-18 11:16:17 +0100

    v1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='opensr-model',
-    version='1.0.0',
+    version='1.0.1',
     author = "Simon Donike, Cesar Aybar, Luis Gomez Chova, Freddie Kalaitzis",
     author_email = "accounts@donike.net",
     description = "ESA OpenSR Diffusion model package for Super-Resolution of Senintel-2 Imagery",
@@ -17,7 +17,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-    'pytorch',
 	'numpy',
 	'einops',
 	'rasterio',
